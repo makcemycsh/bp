2714d5f042fadff7453af7f128fd1cc5cc351909
Максим <maksim@alexandr-808.local>
2018-05-18 10:53:45 +0300

    rework

--- build/js/tabs.js
+++ build/js/tabs.js
@@ -1,96 +1,83 @@
-// $( function() {
-//     $( "#tabs" ).tabs();
-//   } );
 
+$(document).ready(function () {
 
-$(document).ready(function() {
-	console.log(123);
-	$('.js-labels label').click(function() {
-		$('.js-labels label').removeClass('mod-active');
-		$(this).addClass('mod-active');
-	});
+    $('.b-tabs label:not(.b-tabs__js-arrow)').click(function () {
+        $('.b-tabs label:not(.b-tabs__js-arrow)').removeClass('mod-active');
+        $(this).addClass('mod-active');
+    });
 
-	// //Отступ для первого выводимого блока b-task-content
-	// var cur_height = $('.b-task-tabs input:checked+label+.b-task-content').outerHeight(true);
-	// $('.b-task-tabs__inner').css('padding-bottom',cur_height + 160);
+    //Скролл b-tabs__labels при наведении на кнопки
+    var leftArrow = $(".b-tabs__js-arrow_left");
+    var rightArrow = $(".b-tabs__js-arrow_right");
+    var labelsWrap = $('.b-tabs__labels');
 
-	// //Отступ для остальных блоков b-task-content при их переключении
-	// $('.b-task-tabs input').change(function(){
-	// 	cur_height = $('.b-task-tabs input:checked+label+.b-task-content');
-	// 	$('.b-task-tabs__inner').css('padding-bottom',cur_height.outerHeight(true));
+    function scrollLeft() {
+        labelsWrap.animate(
+            {scrollLeft: '-=250'},
+            "500",
+            'linear',
+            scrollLeft
+        );
+    }
 
-	// });	
+    function scrollRight() {
+        labelsWrap.animate(
+            {scrollLeft: '+=250'},
+            "500",
+            'linear',
+            scrollRight
+        );
+    }
 
-	// //Скролл Label .task при наведении на кнопки
-	// function loopl(){
-	// 	$('.b-task-tabs__inner').animate(
-	// 		{scrollLeft: '-=120'},
-	// 		"500",
-	// 		'linear',
-	// 		loopl
-	// 		);
-	// }        
-	// function loopr(){
-	// 	$('.b-task-tabs__inner').animate(
-	// 		{scrollLeft: '+=120'},
-	// 		"500",
-	// 		'linear',
-	// 		loopr  
-	// 		);
-	// }
+    function stop() {
+        labelsWrap.stop();
+    }
 
-	// function stop(){
-	// 	$('.b-task-tabs__inner').stop();
-	// }
+    leftArrow.hover(scrollLeft, stop);
+    rightArrow.hover(scrollRight, stop);
 
-	// $( ".js-arrow.left" ).hover(loopl, stop);
-	// $( ".js-arrow.right" ).hover(loopr, stop);
+    //скрытие кнопки вправо, если блоков меньше ширины обертки
+    function right_nav() {
 
-	// //скрытие кнопки вправо, если блоков меньше ширины обертки 
-	// function right_nav(){
-	// 	var inner_width = 0;
-	// 	var wrap_width = $('.b-task-tabs__inner').width();
+        var innerWidth = 0;
+        var wrapWidth = $('.b-task-tabs__inner').width();
 
-	// 	$('.b-task-tabs__inner label:not(.js-arrow)').each(function(i) {
+        $('.b-tabs__labels span').each(function (i) {
+            innerWidth += $(this).outerWidth(true);
+        });
 
-	// 		if($(this).width()  < 100){
-	// 			inner_width += 100;
-	// 		} else {
-	// 			inner_width += $(this).width();
-	// 		}
-	// 	});
+        if (wrapWidth > innerWidth) {
+            rightArrow.hide();
+        } else {
+            rightArrow.show();
+        }
+    }
 
-	// 	if (wrap_width > inner_width) {
-	// 		$( ".js-arrow.right" ).hide();
-	// 	} else {
-	// 		$( ".js-arrow.right" ).show();
-	// 	}
-	// }
+    //Отображение и скрытие кнопок навигации .js-arrow
+    labelsWrap.scroll(function () {
 
-	// right_nav();
-	// $(window).resize(function() {
-	// 	right_nav();
-	// });
+        var $width = $(this).outerWidth();
+        var $scrollWidth = $(this)[0].scrollWidth;
+        var $scrollLeft = $(this).scrollLeft();
 
-	// //Отображение и скрытие кнопок навигации .js-arrow 
-	// $('.b-task-tabs__inner').scroll( function() {
+        if ($scrollLeft != 0) {
+            leftArrow.show();
+        };
 
-	// 	var $width = $(this).outerWidth();
-	// 	var $scrollWidth = $(this)[0].scrollWidth; 
-	// 	var $scrollLeft = $(this).scrollLeft();
+        if ($scrollWidth - $width <= $scrollLeft) {
+            rightArrow.hide();
+        } else if ($scrollLeft === 0) {
+            leftArrow.hide();
+        }
+        else {
+            rightArrow.show();
+            leftArrow.show();
+        }
+    });
 
-	// 	if ($scrollLeft != 0) {
-	// 		$( ".js-arrow.left" ).show();
-	// 	};
+    right_nav();
+    $(window).resize(function () {
+        right_nav();
+    });
 
-	// 	if ($scrollWidth - $width === $scrollLeft){
-	// 		$( ".js-arrow.right" ).hide();
-	// 	} else if ($scrollLeft===0){
-	// 		$( ".js-arrow.left" ).hide();
-	// 	}
-	// 	else{
-	// 		$( ".js-arrow.right" ).show();
-	// 		$( ".js-arrow.left" ).show();
-	// 	}
-	// });
 });
